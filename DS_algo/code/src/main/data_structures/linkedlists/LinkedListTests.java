b0f0fe9ae8a8990a83de872af872cd1a33e22a96
hemant pande <hemant.pande87@gmailcom>
2022-09-09 18:44:58 +0530

    project structure that's sustainable

---
+++ DS_algo/code/src/main/data_structures/linkedlists/LinkedListTests.java
@@ -0,0 +1,15 @@
+package data_structures.linkedlists;
+
+
+public class LinkedListTests {
+
+
+    public void test() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+}
---
+++ DS_algo/code/src/main/data_structures/linkedlists/Node.java
@@ -0,0 +1,4 @@
+package lib.linkedlists;
+
+public class Node {
+}
