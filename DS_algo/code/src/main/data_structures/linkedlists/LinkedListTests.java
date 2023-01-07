8626a4987db018feabf2e153379748daa8673454
hemant pande <hemant.pande87@gmailcom>
2023-01-07 17:23:03 +0530

    some additions

--- DS_algo/code/src/main/data_structures/linkedlists/LinkedListTests.java
+++ DS_algo/code/src/main/data_structures/linkedlists/LinkedListTests.java
@@ -1,15 +1,19 @@
 package data_structures.linkedlists;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class LinkedListTests {
 
-
+    @Test
     public void test() {
         // arrange
 
         // act
 
         // assert
-
+        Assertions.assertEquals(1, 1);
     }
 }
